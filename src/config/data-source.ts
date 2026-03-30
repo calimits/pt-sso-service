c420c071ddd88360fa5c632028f6e500b059dc32
calimits <calimitsacd@gmail.com>
2026-03-30 08:39:59 -0400

    fix: data source works with db url now

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -7,11 +7,7 @@ import Token from "../features/auth/entities/Token";
 
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT as string),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DB_URL,
     entities: [Token, User],
     migrations: ["src/migrations/*.ts"],
     synchronize: true,
