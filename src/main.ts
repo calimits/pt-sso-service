a1ce550e5233df2da61d20a575190ba3cba49c3e
calimits <calimitsacd@gmail.com>
2026-03-30 09:29:41 -0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  console.log(process.env.DB_URL)
   await connectDB();
   await app.listen(process.env.PORT ?? 3000);
 }
